436ebac3f5fd98c68f5cbb9163d1a0e52298d0b3
Jurnest <kyrdrms@gmail.com>
2025-09-29 12:56:00 +0300

    deat: Retry system is added to redis with polly.

--- src/Infrastructure/Libba.HubTo.Arcavis.Infrastructure.Redis/Extensions/Registration.cs
+++ src/Infrastructure/Libba.HubTo.Arcavis.Infrastructure.Redis/Extensions/Registration.cs
@@ -3,6 +3,7 @@ using Libba.HubTo.Arcavis.Infrastructure.Redis.Services;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 using StackExchange.Redis;
+using Polly;
 
 namespace Libba.HubTo.Arcavis.Infrastructure.Redis.Extensions;
 
@@ -11,9 +12,22 @@ public static class Registration
     public static IServiceCollection AddRedisRegistration(this IServiceCollection services, IConfiguration configuration)
     {
         var connectionString = configuration.GetConnectionString("Redis")
-            ?? throw new ArgumentNullException("Redis connection string was not found.");
+            ?? throw new ArgumentNullException(nameof(configuration), "Redis connection string was not found.");
 
-        services.AddSingleton<IConnectionMultiplexer>(ConnectionMultiplexer.Connect(connectionString));
+        var retryPolicy = Policy
+            .Handle<RedisConnectionException>()
+            .WaitAndRetry(new[]
+            {
+                TimeSpan.FromSeconds(1),
+                TimeSpan.FromSeconds(2),
+                TimeSpan.FromSeconds(4)
+            });
+
+        services.AddSingleton<IConnectionMultiplexer>(sp =>
+        {
+            return retryPolicy.Execute(() => ConnectionMultiplexer.Connect(connectionString));
+        });
+
         services.AddScoped<ICacheService, RedisCacheService>();
 
         return services;
